9fd44ebad261cd4374f7ded691649e870db8aca4
Syed Mona Javed <monajaved93@gmail.com>
2020-11-30 16:55:24 -0800

    third commit

--- breaking_bad/lib/breaking_bad.rb
+++
@@ -1,6 +0,0 @@
-require "breaking_bad/version"
-
-module BreakingBad
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ breaking_bad/lib/environment.rb
@@ -0,0 +1,6 @@
+# require "breaking_bad/version"
+
+# module BreakingBad
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
